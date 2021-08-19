26b8f9c7db1e1ca8adfe4c45d064e8c6df53a2e6
zubairabid <zubairabid1999+github@gmail.com>
2021-08-20 02:29:21 +0530

    Testing layers on soyuz. Basic with 4

--- keyboards/ai03/soyuz/keymaps/soyuz_macro/keymap.c
+++ keyboards/ai03/soyuz/keymaps/soyuz_macro/keymap.c
@@ -17,7 +17,28 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ortho_5x4( /* Base */
-    KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_UNDO, \
+    TO(0), TO(1), TO(2), TO(3), \
+	KC_F13,   KC_F14,   KC_F15,   KC_F16, \
+	KC_F17,   KC_F17,   KC_F18,   KC_F20, \
+	KC_F21,   KC_F22,   KC_F23,   KC_F24, \
+	S(KC_PSCREEN),   KC_PSCREEN,   C(KC_C), C(KC_V)  \
+  ),
+  [1] = LAYOUT_ortho_5x4( /*Navigation*/
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+	KC_MS_BTN1,   KC_MS_BTN2,   KC_MS_BTN3,   KC_MS_WH_UP, \
+	KC_PGDOWN,   KC_UP,   KC_PGUP,   KC_MS_WH_DOWN, \
+	KC_LEFT,   KC_DOWN,   KC_RIGHT,   KC_KP_ASTERISK, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
+  ),
+  [2] = LAYOUT_ortho_5x4( /*Art*/
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+	KC_KP_7,   KC_KP_8,   KC_KP_9,   KC_KP_SLASH, \
+	KC_KP_4,   KC_KP_5,   KC_KP_6,   KC_KP_ASTERISK, \
+	KC_KP_1,   KC_KP_2,   KC_KP_3,   KC_KP_MINUS, \
+	KC_KP_0,   KC_KP_DOT,   KC_NUMLOCK, KC_KP_PLUS  \
+  ),
+  [3] = LAYOUT_ortho_5x4( /*Games*/
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
 	KC_KP_7,   KC_KP_8,   KC_KP_9,   KC_KP_SLASH, \
 	KC_KP_4,   KC_KP_5,   KC_KP_6,   KC_KP_ASTERISK, \
 	KC_KP_1,   KC_KP_2,   KC_KP_3,   KC_KP_MINUS, \
